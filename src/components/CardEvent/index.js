6ea14e797b9b2715ef1f4f80667376f264f7e0be
cterrasid <clarette.terrasi@gmail.com>
2019-06-02 15:57:31 +0200

    Create structure of cardEvent component

--- src/components/CardEvent/index.js
+++ src/components/CardEvent/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const CardEvent = () => {
-  return <p>CardEvent</p>;
+  return (
+    <div className="card__container">
+      {/* <img className="card__image" src={image} alt={description} />
+      <p className="card__event-date">{date}</p>
+      <h2 className="card__event-name">{name}</h2>
+      <p className="card__event-place">{place}</p> */}
+    </div>
+  );
 };
 
 export default CardEvent;
