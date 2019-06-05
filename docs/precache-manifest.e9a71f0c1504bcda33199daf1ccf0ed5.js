fb6071106af520902d8442c09d45e7fd9b92add0
sarayfg <sarayfernandezgarcia@gmail.com>
2019-06-05 09:58:09 +0200

    update docs folder

---
+++ docs/precache-manifest.e9a71f0c1504bcda33199daf1ccf0ed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa3a9ae5aa90ed94468f8ff6d2d9fc5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6822cdcf71ad72b18c30",
+    "url": "./static/css/main.8585490a.chunk.css"
+  },
+  {
+    "revision": "bd9ef6cabe170453d870",
+    "url": "./static/js/2.071c3bac.chunk.js"
+  },
+  {
+    "revision": "6822cdcf71ad72b18c30",
+    "url": "./static/js/main.659446c5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6dc6b4f1d2319faa23fff5001134e80c",
+    "url": "./static/media/mapa.6dc6b4f1.png"
+  }
+]);
